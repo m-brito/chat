4a0f7432c28aa3d7287d7f01fea8ff94815edef7
m-brito <cimauriciot@gmail.com>
2022-11-08 09:45:37 -0300

    Service de notificacoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chat/core/services/notification/push_notification_service.dart';
 import 'package:chat/pages/auth_or_app_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +12,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          headline6: TextStyle(
-            color: Colors.white
-          )
-        ),
-        colorScheme: const ColorScheme.light(
-          primary: Colors.blue,
-          secondary: Colors.grey,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => PushNotificationService(),
         )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            textTheme:
+                const TextTheme(headline6: TextStyle(color: Colors.white)),
+            colorScheme: const ColorScheme.light(
+              primary: Colors.blue,
+              secondary: Colors.grey,
+            )),
+        home: const AuthOrAppPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const AuthOrAppPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
